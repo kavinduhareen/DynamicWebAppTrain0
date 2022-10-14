d1b25141a2eaa573ecc99c012c6482c76b6a14f9
kavinduhareen <114353632+kavinduhareen@users.noreply.github.com>
2022-10-14 22:22:53 +0530

    Add files via upload

---
+++ HellowServlet/src/main/java/org/stdyeasy/servelets/HelloWorld.java
@@ -0,0 +1,38 @@
+package org.stdyeasy.servelets;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * Servlet implementation class HelloWorld
+ */
+public class HelloWorld extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public HelloWorld() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		System.out.println("Hello World !");
+		System.out.println("I am hareen.");
+		
+		response.getWriter().println("<h1> Hello World ! </h1>");
+		response.getWriter().println("<h2> Hello World ! </h2>");
+		
+		//response.getWriter().append("Served at: ").append(request.getContextPath());
+	}
+
+}
